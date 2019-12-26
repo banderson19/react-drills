f02f81519756f7b9802795261c110047c72565d4
Bradford <band8@Bradfords-MacBook-Air.local>
2019-12-26 16:55:50 -0700

    up to 4

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: ['pizza', 'salad', 'roll', 'green beans']
+    };
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods.map((e, index) => {
+      return <h2 key={index}>{e}</h2>
+    });
+    return (
+      <div className="App">{foodsToDisplay}</div>
+    )
+  } 
 }
 
 export default App;
+
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: "",
+      foods: ['pizza', 'salad', 'roll', 'green beans']
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter});
+  }
+  
+  render () {
+    let foodsToDisplay = this.state.foods
+      .filter((e, index) => {
+        return e.includes(this.state.filterString)
+      })
+      .map((e, index) => {
+      return <h2 key={index}>{e}</h2>
+    })
+
+    return (
+      <div className="App">
+        <input onChange = { e => this.handleChange(e.target.value)} type='text'/>
+        {foodsToDisplay}
+      </div>
+    );
+  }
 }
 
 export default App;
